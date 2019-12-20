f297f2e9b9af8ff0a3b6ac2a2462367a13d80578
lifeng <lifeng.luck@qq.com>
2019-12-20 20:51:40 +0800

    添加代理配置

--- zsh/configs/proxy.zsh
+++ zsh/configs/proxy.zsh
@@ -1,1 +1,1 @@
-export https_proxy=http://127.0.0.1:6152;export http_proxy=http://127.0.0.1:6152;export all_proxy=socks5://127.0.0.1:6153
+export https_proxy=http://127.0.0.1:8888;export http_proxy=http://127.0.0.1:8888;export all_proxy=socks5://127.0.0.1:8889
